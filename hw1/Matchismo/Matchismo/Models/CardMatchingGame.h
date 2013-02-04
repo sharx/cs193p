852911afe2449aa5e1980713953d77971d8c5b9f
Josh Wisenbaker <macshome@mac.com>
2013-02-04 12:05:59 -0500

    Naming fixes.

--- hw1/Matchismo/Matchismo/Models/CardMatchingGame.h
+++ hw1/Matchismo/Matchismo/Models/CardMatchingGame.h
@@ -2,8 +2,8 @@
 //  CardMatchingGame.h
 //  Matchismo
 //
-//  Created by Josh Wisenbaker on 2/4/13.
-//  Copyright (c) 2013 Josh Wisenbaker. All rights reserved.
+//  Created by macshome on 2/4/13.
+//  Copyright (c) 2013 macshome. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
--- hw1/Matchismo/Matchismo/Models/CardMatchingGame.m
+++ hw1/Matchismo/Matchismo/Models/CardMatchingGame.m
@@ -2,8 +2,8 @@
 //  CardMatchingGame.m
 //  Matchismo
 //
-//  Created by Josh Wisenbaker on 2/4/13.
-//  Copyright (c) 2013 Josh Wisenbaker. All rights reserved.
+//  Created by macshome on 2/4/13.
+//  Copyright (c) 2013 macshome. All rights reserved.
 //
 
 #import "CardMatchingGame.h"
